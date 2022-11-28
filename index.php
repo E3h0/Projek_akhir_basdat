3fe8280d0eef6a46df5241f1335b0f2ab4de3ba3
E3h0e5 <fathan.x.a.m@gmail.com>
2022-11-28 09:46:35 +0700

    edit file index.php

--- index.php
+++ index.php
@@ -7,19 +7,19 @@ error_reporting(0);
 session_start();
 
 if (isset($_SESSION['username'])) {
-    header("Location: kasus.php");
+    header("Location: dashboard.php");
 }
 
 if (isset($_POST['submit'])) {
     $email = $_POST['email'];
-    $password = md5($_POST['password']);
+    $password = $_POST['password'];
 
-    $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+    $sql = "SELECT * FROM admins WHERE email='$email' AND password='$password'";
     $result = mysqli_query($conn, $sql);
     if ($result->num_rows > 0) {
         $row = mysqli_fetch_assoc($result);
         $_SESSION['username'] = $row['username'];
-        header("Location: kasus.php");
+        header("Location: dashboard.php");
     }else {
         echo "<script>alert('Email atau password Anda salah. Silahkan coba lagi!')</script>";
     }
