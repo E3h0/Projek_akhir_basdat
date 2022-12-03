cc21e66da70485b3a35f6080a24a3e58d45a2e96
E3h0e5 <fathan.x.a.m@gmail.com>
2022-12-03 13:21:10 +0700

    update semua fitur di tambah_item.php

--- tambah_item.php
+++ tambah_item.php
@@ -5,6 +5,8 @@
 <?php
     include "config.php";
     $get_pemilik = mysqli_query($conn, "SELECT * FROM pengguna");
+    $get_cat = mysqli_query($conn, "SELECT * FROM category");
+    $get_stat = mysqli_query($conn, "SELECT * FROM status");
 ?>
 <body style="font-family:arial">
  <center><h2>Tambah Data Baru</center>
@@ -14,10 +16,6 @@
 
     <form action="tambah_item.php" method="post" name="form1">
         <table width="100%" border="0">
-            <tr> 
-                <td>Nama Barang</td>
-                <td><input type="text" name="item_name" size="50" required></td>
-            </tr>
             <tr> 
                 <td>Nama Pemilik</td>
                 <!-- <td><input type="text" name="nama_pemilik" size="50" required></td> -->
@@ -33,12 +31,48 @@
                 </td>
             </tr>
             <tr> 
+                <td>Nama Barang</td>
+                <td><input type="text" name="item_name" size="50" required></td>
+            </tr>
+            <tr> 
+                <td>Kategori</td>
+                <!-- <td><input type="text" name="nama_pemilik" size="50" required></td> -->
+                <td>
+                <label for="kategori"></label>
+                <select name="kategori" id="kategori">
+                    <?php
+                        while($cat = mysqli_fetch_array($get_cat)) {
+                            echo "<option value=".$cat['category_name'].">".$cat['category_name']."</option>";
+                        }
+                    ?>
+                </select>
+                </td>
+            </tr>
+            <tr> 
+                <td>Detail</td>
+                <td><textarea rows="5" name="detail" required></textarea></td>
+            </tr>
+            <tr> 
                 <td>lokasi</td>
                 <td><input type="text" name="lokasi_hilang" size="50" required></td>
             </tr>
             <tr> 
                 <td>waktu</td>
                 <td><input type="datetime-local" name="waktu_hilang" size="50" required></td>
+            </tr>
+            <tr> 
+                <td>Status</td>
+                <!-- <td><input type="text" name="nama_pemilik" size="50" required></td> -->
+                <td>
+                <label for="st"></label>
+                <select name="st" id="st">
+                    <?php
+                        while($cat = mysqli_fetch_array($get_stat)) {
+                            echo "<option value=".$cat['s_name'].">".$cat['s_name']."</option>";
+                        }
+                    ?>
+                </select>
+                </td>
             </tr>
             <tr>    
                 <td></td>
@@ -55,14 +89,22 @@
         $nama_pemilik = $_POST['nama_pemilik'];
         $lokasi = $_POST['lokasi_hilang'];
         $waktu = $_POST['waktu_hilang'];
+        $cat = $_POST['kategori'];
+        $st = $_POST['st'];
+        $det = $_POST['detail'];
 
 
         // include database connection file
         include "config.php";
 
         // Insert user data into table
-        $tambah_item = "insert into items(`pengguna_id`, `item_name`, `lokasi_hilang`, `waktu_hilang`) 
-        values((select id from pengguna where pengguna.username =  '$nama_pemilik'), '$nama_barang', '$lokasi', '$waktu')";
+        $tambah_item = "insert into items(`pengguna_id`, `item_name`, `kategori_id`, `lokasi_hilang`, `waktu_hilang`, `st_id`, `detail`) 
+        values
+        (
+        (select id from pengguna where pengguna.username =  '$nama_pemilik'), '$nama_barang', 
+        (select c_id from category where category.category_name ='$cat'),'$lokasi', '$waktu',
+        (select s_id from status where status.s_name = '$st'), '$det'
+        )";
         $kerjakan=mysqli_query($conn, $tambah_item);
         if($kerjakan)
         {
