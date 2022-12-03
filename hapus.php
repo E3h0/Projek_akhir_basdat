0be253d5980b6890a26dc190019461291a532418
E3h0e5 <fathan.x.a.m@gmail.com>
2022-12-03 13:14:51 +0700

    update semua fitur di hapus.php

--- hapus.php
+++ hapus.php
@@ -6,9 +6,14 @@ include "config.php";
 $id = $_GET['id'];
 
 // Delete user row from table based on given id
-$result = mysqli_query($conn, "DELETE items.*
-FROM pengguna INNER JOIN items ON pengguna.id=items.pengguna_id
-WHERE (pengguna.id = $id AND pengguna.id=items.pengguna_id)");
+// $result = mysqli_query($conn, "DELETE items.*
+// FROM pengguna INNER JOIN items ON pengguna.id=items.pengguna_id
+// WHERE (pengguna.id = $id AND pengguna.id=items.pengguna_id)");
+
+$res = mysqli_query($conn, "DELETE pengguna.*, items.*
+FROM pengguna, items WHERE (pengguna.id = $id AND pengguna.id=items.pengguna_id)");
+
+
 
 // After delete redirect to Home, so that latest user list will be displayed.
 header("Location:index.php");
